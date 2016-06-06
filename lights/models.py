41d80e048ee1f0411f308582b0f8d50b909545a2
George Griffin <george@Georges-MacBook-Pro.local>
2016-06-06 15:22:36 -0400

    chchchchchanges!

--- lights/models.py
+++ lights/models.py
@@ -14,10 +14,12 @@ Strand = pypurdypixels.Strand
 class LightManager(models.Manager):
     def get_strand(self):
         num_pixels = matrix_settings.strand_length
-        debug = matrix_settings.debug
-        self.get_queryset().values('position', 'color')
-        pixels = map(Strand.rgb2px, )
-        strand = Strand(strand_length=num_pixels, debug=debug)
+        debug_mode = matrix_settings.debug
+        lights = self.get_queryset()
+        pixels = map(lambda x: Strand.rgb2px(*x.color_as_pixel), lights)
+        strand = Strand(strand_length=num_pixels, pixels=pixels,
+                        debug_mode=debug_mode)
+        return strand
 
     def get_matrix(self):
         cols = matrix_settings.cols
@@ -33,7 +35,9 @@ class Light(models.Model):
     position = models.IntegerField(unique=True)
     assigned_user = models.ForeignKey(User, null=True, blank=True)
     color = RGBColorField()
+    objects = LightManager()
 
+    @property
     def color_as_pixel(self):
         r = self.color[1:3]
         g = self.color[3:5]
