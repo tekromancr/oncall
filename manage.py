cf3a06e7d263db84739edbe638d18d7059d07cbb
George Griffin <george@Georges-MacBook-Pro.local>
2016-06-18 02:08:27 -0400

    creates dev/pi modes, starts work on react implementation

--- manage.py
+++ manage.py
@@ -1,9 +1,12 @@
-#!/usr/bin/env python
+#!./venv/bin/python
 import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oncall.settings")
+    if os.path.split(sys.argv[0])[1] == "manage.py":
+      os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oncall.dev_settings")
+    else:
+      os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oncall.pi_settings")
 
     from django.core.management import execute_from_command_line
 
